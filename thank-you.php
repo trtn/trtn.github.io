364dcc11cdfc5fe3daacd58167f7227b3ca8c6aa
Yannis Flet-Berliac <writetotrtn@gmail.com>
2017-10-05 12:23:35 +0200

    changed background and back to front page

--- thank-you.php
+++ thank-you.php
@@ -1,4 +1,4 @@
-<?php if($_POST['uxbqsvpgyr']=='wjeiotvapoyavfe'&&$_POST['qgdvdt']==''){$m='Request:';$h="From: noreply@example.com\r\nContent-Type: text/plain; charset=UTF-8\r\n";mail('writetotrtn@gmail.com','Mail send love',stripslashes($m),$h);} if($_POST['uxbqsvpgyr']=='wjeiotvapoyavfe'&&$_POST['qgdvdt']==''){$m='Request:';$h="From: noreply@example.com\r\nContent-Type: text/plain; charset=UTF-8\r\n";foreach($_POST['message'] as $av){$m.="\nmessage: ".$av;}mail('writetotrtn@gmail.com','Spontatenous message',stripslashes($m),$h);} if($_POST['uxbqsvpgyr']=='wjeiotvapoyavfe'&&$_POST['qgdvdt']==''){$m='Request:';$h="From: noreply@example.com\r\nContent-Type: text/plain; charset=UTF-8\r\n";foreach($_POST['e-mail'] as $av){$m.="\ne-mail: ".$av;}mail('writetotrtn@gmail.com','Subscription',stripslashes($m),$h);} ?><!DOCTYPE html>
+<?php if($_POST['ugcalfp']=='cofvrrycupeytneqlos'&&$_POST['tdiamy']==''){$m='Request:';$h="From: noreply@example.com\r\nContent-Type: text/plain; charset=UTF-8\r\n";mail('writetotrtn@gmail.com','Mail send love',stripslashes($m),$h);} if($_POST['flakbj']=='fuvadvfwlxgg'&&$_POST['aovqnd']==''){$m='Request:';$h="From: noreply@example.com\r\nContent-Type: text/plain; charset=UTF-8\r\n";foreach($_POST['message'] as $av){$m.="\nmessage: ".$av;}mail('writetotrtn@gmail.com','Spontatenous message',stripslashes($m),$h);} if($_POST['ujsdaxlufrp']=='dkitbhmpwaqy'&&$_POST['fvydcdjdym']==''){$m='Request:';$h="From: noreply@example.com\r\nContent-Type: text/plain; charset=UTF-8\r\n";foreach($_POST['e-mail'] as $av){$m.="\ne-mail: ".$av;}mail('writetotrtn@gmail.com','Subscription',stripslashes($m),$h);} ?><!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
@@ -22,24 +22,24 @@
 <meta name="viewport" content="width=960">
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Muli%7CMontserrat:300,400&amp;subset=all">
 <title>Thank you</title>
-<link rel="stylesheet" type="text/css" href="css/site.20170929122529.css">
+<link rel="stylesheet" type="text/css" href="css/site.20171005122249.css">
 <!--[if lte IE 7]>
-<link rel="stylesheet" type="text/css" href="css/site.20170929122529-lteIE7.css">
+<link rel="stylesheet" type="text/css" href="css/site.20171005122249-lteIE7.css">
 <![endif]-->
 </head>
-<body id="body">
-<div class="vis pos size cont-37">
-<div class="pos-35 vis-2 section">
-<div class="vis pos-7 size-66 cont-4">
-<p class="para-12"><span class="font-20"><a href="index.html#anchor1">Go b</a></span><span class="font-20"><a href="index.html#anchor1">ack to main stage.</a></span></p>
+<body id="body-2">
+<div class="vis pos size-64 cont-33">
+<div class="pos-36 vis-2 section-3">
+<div class="vis pos-9 size-57 cont-4">
+<p class="para-12"><span class="font-20"><a href="index.html#anchor1">&larr;</a></span></p>
 </div>
-<div class="vis pos-47 size-67 cont-30">
+<div class="vis pos-48 size-66 cont-34">
 <p class="para-13"><span class="font-21">Thank you for your love. Stay tuned!</span></p>
 </div>
 </div>
 </div>
 <script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="js/thank-you.20170929122529.js"></script>
+<script type="text/javascript" src="js/thank-you.20171005122249.js"></script>
 <script type="text/javascript">
 var ver=RegExp(/Mozilla\/5\.0 \(Linux; .; Android ([\d.]+)/).exec(navigator.userAgent);if(ver&&parseFloat(ver[1])<5){document.getElementsByTagName('body')[0].className+=' whitespacefix';}
 </script>
